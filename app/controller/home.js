b88d82fc7c89b1bd2f4ca75cd00bbea5c990ba6e
zhuxinxin <zhuxinxin@xiaomei.com>
2016-01-06 12:15:59 +0800

    login btn style changed

--- app/controller/home.js
+++ app/controller/home.js
@@ -12,7 +12,6 @@ router.get('/', function (req, res, next) {
   var sessionId = req.cookies.sid;
   User.find({sid: sessionId}, function (err, user) {
     if (err) return next(err);
-    console.log(user);
     res.render('index', {
       title: 'Generator-Express MVC OLYMPUS',
       user: user[0]
