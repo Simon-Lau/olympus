55df45347bb3f2e5d06221cba9e6de5c1d840d7d
zhuxinxin <zhuxinxin@xiaomei.com>
2016-02-19 17:52:02 +0800

    aliyun config

--- config/config.js
+++ config/config.js
@@ -28,6 +28,15 @@ var config = {
     },
     port: process.env.PORT || 3000,
     db: 'mongodb://catlike:123456@ds035333.mongolab.com:35333/olympus'
+  },
+
+  aliyun: {
+    root: rootPath,
+    app: {
+      name: 'olympus'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost:27017/olympus'
   }
 };
 
